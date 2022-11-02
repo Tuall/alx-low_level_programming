daf8c67d83b83e9a97ee5de829937913dce076fa
root <uperpetual3@gmail.com>
2022-11-02 05:55:09 -0700

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,16 @@
-#ifndef FILE_IO
+#ifndef MAIN_H
 
-#define FILE_IO
+#define MAIN_H
 
 
 
-#include <sys/types.h>
+#include <stdio.h>
+
+#include <stdlib.h>
 
 #include <sys/stat.h>
+
+#include <sys/types.h>
 
 #include <fcntl.h>
 
@@ -19,6 +23,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
+
+int _strlen(char *str);
 
 int append_text_to_file(const char *filename, char *text_content);
 
