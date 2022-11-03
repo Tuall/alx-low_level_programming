bd9d6ec0030f8f09905e69f6e02241820b973644
root <uperpetual3@gmail.com>
2022-11-03 03:46:54 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,51 @@
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include "holberton.h"
+#include "main.h"
+#include <stddef.h>
 
 /**
- * append_text_to_file - that appends text at the end of a file
- * @filename: variable pointer
- * @text_content: content file
- * Description: function that appends text at the end of a file
- * Return: 1 on success, -1 on failure
+ * _strlen - counts string length
+ * @str: string to be used
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+		
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: content to be appended
+ *
+ * Return: 1 on success, -1 otherwise
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
-
+	int file, wrote;
+			
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content == NULL)
-		text_content = "";
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
 	file = open(filename, O_WRONLY | O_APPEND);
-
 	if (file == -1)
 		return (-1);
-
-	write(file, text_content, i);
-
-	return (1);
+	if (text_content != NULL)
+	{
+		wrote = write(file, text_content, _strlen(text_content));
+		if (wrote == -1)
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
 }
