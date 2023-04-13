c25aec105316513d5ee07d9fb2262fbaf4112083
Tuall <111268805+Tuall@users.noreply.github.com>
2023-04-13 13:30:00 +0100

    task

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -34,4 +34,11 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%d] and [%d]\n", prev, index);
 
 	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
 
+	return (-1);
+}
